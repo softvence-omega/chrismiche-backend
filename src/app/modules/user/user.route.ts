32e215217b355110067e28a24cee377c3bb625b7
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-05-18 14:33:26 +0600

    bugs fixed

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get("/:id", UserControllers.getSingleUser);
 router.get("/", UserControllers.getAllUsers);
 router.post(
-  "/createAUser",
+  "/createUser",
   validateRequest(UserValidations.createUserValidationSchema),
   UserControllers.createAUser
 );
