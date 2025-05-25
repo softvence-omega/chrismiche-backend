6689a5f705182046900db29bb87cc0aff224c4fd
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-05-25 23:01:28 +0600

    user interface changed

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,22 +1,20 @@
-import {  Model } from "mongoose";
+import { Model } from "mongoose";
 import USER_ROLE from "../../constants/userRole";
 import { TCharacter } from "../../config/characters";
 
 export type TRegisterUserInput = Pick<TUser, "email" | "password">;
 
 export interface TUser {
-  fullName: string
+  fullName: string;
   email: string;
   username: string;
-  gender: string
+  gender: string;
   password: string;
-  // confirmPassword: string;
-  // image: string;
   phoneNumber: string;
   character: TCharacter; // strictly typed
   role: "admin" | "user";
-  passwordResetToken?: string | null
-  passwordResetExpires?: Date | null
+  passwordResetToken?: string | null;
+  passwordResetExpires?: Date | null;
 
   isDeleted: boolean;
 }
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -3,19 +3,18 @@ import { CharacterList } from "../../config/characters";
 
 const createUserValidationSchema = z.object({
   body: z.object({
-    email: z.string({ required_error: "Email is required" }).email("Invalid email format"),
+    email: z
+      .string({ required_error: "Email is required" })
+      .email("Invalid email format"),
     password: z
       .string({ required_error: "Password is required" })
       .min(6, "Password must be at least 6 characters")
       .max(20, "Password must be at most 20 characters"),
-    fullName: z.string().optional(), 
+    fullName: z.string().optional(),
     username: z.string().optional(),
-    gender: z.string().optional(),
-    // image: z.string().optional(),
     phoneNumber: z.string().optional(),
+    gender: z.enum(["male", "female", "other"]).optional(),
     character: z.enum(CharacterList).default("Robo"),
-
-    
     role: z.enum(["admin", "user"]).default("user"),
   }),
 });
