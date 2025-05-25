bcff37be2c59f7ab0a283d09be4f40a7707adbed
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-05-26 04:01:18 +0600

    movement model created

---
+++ src/app/modules/movement/movement.model.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+import { TMovement } from "./movement.interface";
+
+const movementSchema = new Schema<TMovement>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    distance: {
+      type: Number,
+      required: true,
+    },
+    floorsClimbed: {
+      type: Number,
+      required: true,
+    },
+    activityDate: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Movement = model<TMovement>("Movement", movementSchema);
