099d7b848abf348a79298f77a125a59f6fd76c14
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-05-28 05:56:21 +0600

    updated version

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { UserControllers } from "./user.controller";
 import { validateRequest } from "../../middleWear/validateRequest";
 import { UserValidations } from "./user.validation";
-import auth from "@/app/middleWear/auth";
+import auth from "../../middleWear/auth";
 
 const router = express.Router();
 
